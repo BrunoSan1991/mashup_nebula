443b591e267d17f00e7e55f5f4cb5dfa870b5258
unknown <bsan.designgrafico@gmail.com>
2024-11-13 19:19:21 -0300

    renderizano pie-chart

--- src/configure.js
+++ src/configure.js
@@ -1,6 +1,7 @@
 import { embed } from '@nebula.js/stardust';
 
 import barchart from '@nebula.js/sn-bar-chart';
+import piechart from '@nebula.js/sn-pie-chart';  // Importando o gráfico de pizza
 
 const n = embed.createConfiguration({
   context: {
@@ -11,6 +12,10 @@ const n = embed.createConfiguration({
     {
       name: 'barchart',
       load: () => Promise.resolve(barchart),
+    },
+    {
+      name: 'piechart',
+      load: () => Promise.resolve(piechart),  // Registrando o gráfico de pizza
     },
   ],
 });
